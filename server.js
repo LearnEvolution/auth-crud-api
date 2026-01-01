25eaf6662adbe8589d3d543a498dd73a4f62fb4f
psh.evolution <psh.evolution@gmail.com>
2026-01-01 01:10:09 -0300

    fix: cors for vercel

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ import itemRoutes from "./src/routes/itemRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 // 🔑 PADRÃO ÚNICO
